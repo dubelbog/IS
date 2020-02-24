6098d056fa03211aea9048ed6cda986b33ce0d22
Bogumila Dubel <bogumila.dubel@namics.com>
2020-02-24 17:41:04 +0100

    lab 01 first commit

--- src/main/java/ch/zhaw/its/lab/secretkey/TotallySecureRandom.java
+++ src/main/java/ch/zhaw/its/lab/secretkey/TotallySecureRandom.java
@@ -6,10 +6,13 @@ public class TotallySecureRandom extends SecureRandom {
     @Override
     public void nextBytes(byte[] bytes) {
         long now = System.currentTimeMillis();
-
+        System.out.println("now: " + now);
+        System.out.println("bytes.length: " + bytes.length);
         for (int i = 0; i < bytes.length; i++) {
             bytes[i] = (byte) (now & 0xff);
+            System.out.println(bytes[i]);
             now >>= 8;
+            System.out.println("now new: " + now);
         }
     }
 }
